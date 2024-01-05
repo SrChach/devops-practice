84a76c0ea8b6a1c0e4b4b3c83dc7df92ade0949b
ignacio.martinez.avila <ignacio.martinez.avila.contractor@bbva.com>
2024-01-05 04:09:39 -0600

    ci: added data source

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  whitelist_cidr = ["0.0.0.0/0"]
+}
+
 resource "aws_s3_bucket" "example_bucket" {
     bucket = var.bucket_name
 
@@ -5,6 +9,32 @@ resource "aws_s3_bucket" "example_bucket" {
         Environment = var.environment
         Project = var.project_name
     }
+}
+
+resource "aws_iam_policy_document" "main_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+        "s3:GetObject"
+    ]
+    resources = [
+        "${aws_s3_bucket.example_bucket.arn}/*"
+    ]
+    condition {
+      test = "IpAddress"
+      variable = "aws:SourceIp"
+      values = local.whitelist_cidr
+    }
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "main_policy_bucket" {
+  bucket = aws_s3_bucket.example_bucket.id
+  policy = data.aws_iam_policy_document.main_policy.json
 }
 
 resource "aws_s3_object" "initial_html_file" {
