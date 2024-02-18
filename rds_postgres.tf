52b6f7a52fc196bba5e7886f34293e9409d65808
Ignacio Martinez <gsikmo@gmail.com>
2024-02-18 16:46:46 +0000

    CI: Added security group for rds

--- rds_postgres.tf
+++ rds_postgres.tf
@@ -1,3 +1,19 @@
+resource "aws_security_group" "rds_sg" {
+  name = "rds_sg"
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_instance" "ropa-postgres-db" {
   allocated_storage    = 10
   engine               = "postgres"
@@ -9,7 +25,15 @@ resource "aws_db_instance" "ropa-postgres-db" {
   storage_encrypted    = false
   publicly_accessible  = true
   username             = var.rds_ropa_username
+  vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   apply_immediately    = true
+}
+
+output "security_group_id" {
+  value       = aws_security_group.rds_sg.id
+}
+output "db_instance_endpoint" {
+  value       = aws_db_instance.ropa-postgres-db.endpoint
 }
 
 /*
